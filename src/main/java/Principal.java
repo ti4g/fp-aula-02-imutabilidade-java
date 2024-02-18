4ba6e5115f5860046f8c639e97f2a722a4ad3976
Manoel Campos <manoelcampos@gmail.com>
2024-02-18 17:14:21 -0300

    Commit inicial
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

---
+++ src/main/java/Principal.java
@@ -0,0 +1,9 @@
+/**
+ * Classe para você testar suas implementações.
+ * Veja o README para mais detalhes.
+ */
+public class Principal {
+    public static void main(final String[] args) {
+
+    }
+}
