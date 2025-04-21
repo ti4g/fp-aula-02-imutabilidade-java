55ad1128c1cd5886d9c0c5de97d59f24829341f3
ti4g <tiagosousaquixabaaa@gmail.com>
2025-04-21 15:33:20 -0300

    Update Records e Class

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -2,8 +2,18 @@
  * Classe para você testar suas implementações.
  * Veja o README para mais detalhes.
  */
+
+import exercicios.CidadeRecord;
+import exercicios.PessoaRecord;
+import exercicios.PessoaRecordShallow;
+import exercicios.Cidade;
+
 public class Principal {
     public static void main(final String[] args) {
 
+        CidadeRecord Tocantins = new CidadeRecord("Palmas");
+        Cidade Palmas = new Cidade("Palmas");
+        PessoaRecordShallow pessoa = new PessoaRecordShallow("Tiago", São Paulo);
+        PessoaRecord pessoa2 = new PessoaRecord("Paulo", Rio de Janeiro  );
     }
 }
